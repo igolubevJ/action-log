c31a016e68fb0886838048db9f745c2105c5397c
IGolubevJ <igolubevapp@gmail.com>
2021-10-25 05:22:27 -0400

    Implement Router for projects endpoints

--- src/app.js
+++ src/app.js
@@ -1,11 +1,15 @@
 const express = require('express');
 
+const { currentUser } = require('./middleware/current-user.middleware');
+
 const projectRouter = require('./routes/project.router');
 
 const createServer = () => {
   const app = express();
 
   app.use(express.json());
+
+  app.use(currentUser);
 
   app.use('/api', projectRouter);
 
--- src/routes/project.router.js
+++ src/routes/project.router.js
@@ -1,31 +1,42 @@
 const { Router } = require('express');
 
+const ProjectRepo = require('../repos/project.repo');
+
 const router = new Router();
 
-router.get('/projects', (req, res) => {
-  res.send('GET /api/projects - not implement')
+router.get('/projects', async (req, res) => {
+  const result = await ProjectRepo.find();
+
+  res.json(result);
 });
 
-router.get('/projects/:id', (req, res) => {
+router.get('/projects/:id', async (req, res) => {
   const { id } = req.params;
-  res.send(`GET /api/projects/${id} - not implement`);
+  const result = await ProjectRepo.findById(id);
+
+  res.json(result);
 });
 
-router.post('/projects', (req, res) => {
+router.post('/projects', async (req, res) => {
   const { title } = req.body;
-  res.send(`POST /api/projects { title: ${title} } - not implement`);
+  const result = await ProjectRepo.create(title, req.user.id);
+  res.json(result);
 });
 
-router.put('/projects/:id', (req, res) => {
+router.put('/projects/:id', async (req, res) => {
   const { id } = req.params;
   const { title } = req.body;
 
-  res.send(`PUT /api/projects/${id} { title: ${title} } - not implement`);
+  const result = await ProjectRepo.update(id, title);
+
+  res.json(result);
 });
 
-router.delete('/projects/:id', (req, res) => {
+router.delete('/projects/:id', async (req, res) => {
   const { id } = req.params;
-  res.send(`DELETE /api/projects/${id} - not implement`);
+  const result = await ProjectRepo.delete(id);
+
+  res.json(result);
 });
 
 module.exports = router;
