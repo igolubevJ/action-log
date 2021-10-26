f16187bb96dd94c8b755b0df5450ca1d7903b2aa
IGolubevJ <igolubevapp@gmail.com>
2021-10-26 02:00:24 -0400

    Add Mission validator

--- src/routes/mission.router.js
+++ src/routes/mission.router.js
@@ -1,4 +1,5 @@
 const { Router } = require('express');
+const { check } = require('express-validator');
 
 const {
   find,
@@ -10,13 +11,37 @@ const {
 
 const router = Router();
 
-router.get('/missions', find);
+router.get('/missions',find);
 
 router.get('/missions/:id', findById);
 
-router.post('/missions', create);
+router.post(
+  '/missions',
+  check('title')
+    .isLength({ min: 3 })
+    .withMessage('Min length title is 3 characters')
+    .isLength({ max: 500 })
+    .withMessage('Max length title is 500 characters'),
+  check('projectId')
+    .not()
+    .isEmpty()
+    .withMessage('Project does not exist'),
+  create
+);
 
-router.put('/missions/:id', update);
+router.put(
+  '/missions/:id',
+  check('title')
+    .isLength({ min: 3 })
+    .withMessage('Min length title is 3 characters')
+    .isLength({ max: 500 })
+    .withMessage('Max length title is 500 characters'),
+  check('projectId')
+    .not()
+    .isEmpty()
+    .withMessage('Project does not exist'),
+  update
+);
 
 router.delete('/missions/:id', destroy);
 
